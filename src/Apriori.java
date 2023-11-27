c22b82f805977246955b4989b388452d3360b362
Florian Gramsch <florigram@gmail.com>
2023-11-27 13:01:37 +0100

    apriori plus tree like print

---
+++ src/Apriori.java
@@ -0,0 +1,147 @@
+package src;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static src.Main.print_array;
+
+public class Apriori {
+    private final List<List<Integer>> data;
+    private List<List<Integer>> positive_border;
+    private List<List<Integer>> negative_border;
+    private List<List<Integer>> frequent_set;
+    public Apriori(List<List<Integer>> data) {
+        this.data = data;
+        this.positive_border = new ArrayList<>();
+        this.negative_border = new ArrayList<>();
+        this.frequent_set = new ArrayList<>();
+    }
+
+    public float support(List<Integer> this_set) {
+        int count = 0;
+        for (List<Integer> row : this.data) {
+            boolean supported = true;
+            for (int val : this_set) {
+                boolean tmp = row.get(val) == 1;
+                supported = supported && tmp;
+            }
+            if (supported) {
+                count++;
+            }
+        }
+//        System.out.print("Total count of (");
+//        for (int x : this_set) {
+//            System.out.print(x);
+//        }
+//        System.out.println("): " + count);
+        return (float) count / data.size();
+    }
+
+    private List<List<Integer>> generate(List<List<Integer>> candidates) {
+        int n = candidates.size();
+        int num = candidates.get(0).size();
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++){
+                int a = candidates.get(i).get(num-1);
+                int b = candidates.get(j).get(num-1);
+                List<Integer> base_a = new ArrayList<>(candidates.get(i).subList(0, candidates.get(i).size() - 1));
+                List<Integer> base_b = new ArrayList<>(candidates.get(j).subList(0, candidates.get(j).size() - 1));
+                ;
+                if (base_a.equals(base_b)) {
+                    List<Integer> tmpList = new ArrayList<>(base_a);
+                    tmpList.add(a);
+                    tmpList.add(b);
+                    result.add(tmpList);
+                }
+            }
+        }
+        return result;
+    }
+
+    private List<List<Integer>> prune(List<List<Integer>> to_prune, double k) {
+        List<List<Integer>> pruned = new ArrayList<>();
+        for (List<Integer> can : to_prune) {
+//            for (int x : can) {
+//                System.out.print(x);
+//            }
+            if (this.support(can) >= k) {
+                pruned.add(can);
+            }
+            else {
+                this.negative_border.add(can);
+            }
+        }
+        return pruned;
+    }
+
+    public void run(double k) {
+        this.frequent_set = new ArrayList<>();
+        this.negative_border = new ArrayList<>();
+        this.positive_border = new ArrayList<>();
+        List<List<Integer>> c = new ArrayList<>();
+        for (int i=0; i<this.data.get(0).size(); i++) {
+            int finalI = i;
+            c.add(new ArrayList<>(){{
+                add(finalI);
+            }});
+        }
+        c = this.prune(c, k);
+        while (!c.isEmpty()) {
+            this.frequent_set.addAll(c);
+            c = this.generate(c);
+            c = this.prune(c, k);
+        }
+    }
+
+    public List<List<Integer>> getf() {
+        return this.frequent_set;
+    }
+    public List<List<Integer>> getNegative() {
+        return this.negative_border;
+    }
+
+    public void show(float start, float end, float step) {
+        for (float i = start; i <= end; i=i+step) {
+            this.run(i);
+            System.out.println("k: " + i);
+            System.out.println("Frequent Set: ");
+            print_array(this.data);
+            System.out.println("Frequent Set: ");
+            print_array(this.getf());
+            System.out.println("Negative Border: ");
+            print_array(this.getNegative());
+        }
+    }
+
+    public void tree() {
+        List<List<List<Integer>>> patterns = new ArrayList<>();
+        List<List<Integer>> c = new ArrayList<>();
+        for (int i = 0; i < this.data.get(0).size(); i++) {
+            int finalI = i;
+            c.add(new ArrayList<>() {{
+                add(finalI);
+            }});
+        }
+        while (!c.isEmpty()) {
+            patterns.add(c);
+            c = generate(c);
+        }
+        for (List<List<Integer>> x : patterns) {
+            for (List<Integer> y : x) {
+                System.out.print("( ");
+                for (int z : y) {
+                    System.out.print(z);
+                }
+                System.out.print(": " + this.support(new ArrayList<>(y)));
+                System.out.print(" )");
+            }
+            System.out.println();
+        }
+    }
+}
+//      {}
+//   0  1  2  3
+// 01 02 03 12 13 23
+//   012 013 123
+//      0123
--- src/Main.java
+++ src/Main.java
@@ -5,52 +5,39 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.util.stream.Stream;
+import java.util.List;
 
 public class Main {
-    public static int getlines(String fileName) {
-        Path path = Paths.get(fileName);
-        int lines = 0;
-        try {
-            lines = (int) Files.lines(path).count();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return lines;
-    }
-    public static int[][] read_file(String path) {
-        int[][] dm;
+    public static List<List<Integer>> read_file(String path) {
+        List<List<Integer>> data = new ArrayList<>();
         try {
             File file = new File(path);
             Scanner scanner = new Scanner(file);
-            int length = getlines(path);
-            dm = new int[length][];
 
-            int counter = 0;
             while (scanner.hasNextLine()) {
                 String[] teile = scanner.nextLine().split(",");
-                int[] tmp = new int[teile.length];
-                for (int i = 0; i < teile.length; i++) {
-                    tmp[i] = Integer.parseInt(teile[i]);
+                List<Integer> tmp = new ArrayList<>();
+                for (String s : teile) {
+                    tmp.add(Integer.parseInt(s));
                 }
-                dm[counter] = tmp;
-                counter++;
+                data.add(tmp);
             }
             scanner.close();
-            return dm;
+            return data;
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            return new int[][]{{-1}};
+            return new ArrayList<>() {{add(new ArrayList<>() {{ add(-1);}});}};
         }
     }
 
-    public static void print_array(int[][] data) {
+    public static void print_array(List<List<Integer>> data) {
         System.out.println("[");
-        for (int[] x : data) {
+        for (List<Integer> x : data) {
             System.out.print(" [ ");
             for (int y : x) {
                 System.out.print(y + " ");
@@ -59,25 +46,7 @@ public class Main {
         }
         System.out.println("]");
     }
-    public static float support(int[][] data, int[] this_set) {
-        int count = 0;
-        for (int[] row : data) {
-            boolean supported = true;
-            for (int val : this_set) {
-                boolean tmp = row[val] == 1;
-                supported = supported && tmp;
-            }
-            if (supported) {
-                count++;
-            }
-        }
-//        System.out.print("Total count of (");
-//        for (int x : this_set) {
-//            System.out.print(x);
-//        }
-//        System.out.println("): " + count);
-        return (float) count / data.length;
-    }
+
 
     public static int[] concat_int(int[] a, int[] b ) {
         int len_a = a.length;
@@ -87,70 +56,19 @@ public class Main {
         System.arraycopy(b, 0, result, len_a, len_b);
         return result;
     }
-    public static int[][] generate(int[][] candidates) {
-        int n = candidates.length;
-        int num = candidates[0].length;
-        int maybe_length = (n*(n-1)/2);
-        int[][] result = new int[maybe_length][];
-        int counter = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++){
-                int[] a = {candidates[i][num - 1]};
-                int[] b = {candidates[j][num - 1]};
-                int[] base_a = new int[num-1];
-                int[] base_b = new int[num-1];
-                System.arraycopy(candidates[i], 0, base_a, 0, num-1);
-                System.arraycopy(candidates[j], 0, base_b, 0, num-1);
-                if (Arrays.equals(base_a, base_b)) {
-                    result[counter] = concat_int(concat_int(base_a, a), b);
-                    counter++;
-                }
-            }
-        }
-        result = Arrays.copyOf(result, counter);
-        return result;
-    }
 
-    public static int[][] prune(int[][] data, int[][] to_prune, double k) {
-        int[][] pruned = new int[to_prune.length][];
-        int counter = 0;
-        for (int[] can : to_prune) {
-//            for (int x : can) {
-//                System.out.print(x);
-//            }
-            if (support(data, can) >= k) {
-                pruned[counter] = can;
-                counter++;
-            }
-        }
-        pruned = Arrays.copyOf(pruned, counter);
-        return pruned;
-    }
-
-
-//    public static int[][] apriori(int[][] data) {
-//        int[][] result = new int[10][];
-//
-//
-//        return result;
-//    }
 
     public static void main(String[] args) {
-        int[][] data1 = read_file("src/dm1.csv");
-        int[][] data2 = read_file("src/dm2.csv");
-        int[][] test = {
-                {0, 0, 1},
-                {1, 1, 0},
-                {1, 0, 0}
-        };
-//        float support = support(data1, new int[] {0, 2, 3});
-//        System.out.println("Support: " + support);
-        int[][] cands = new int[][]{{0}, {1}, {2}};
-        cands = generate(cands);
-        int[][] pruning = prune(test, cands, 0.3);
-//        int[][] to_gen = new int[][]{{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}};
-//        int[][] cands = generate(to_gen);
-//        cands = generate(cands);
-        print_array(pruning);
+        List<List<Integer>> data1 = read_file("src/dm1.csv");
+        List<List<Integer>> data2 = read_file("src/dm2.csv");
+        List<List<Integer>> test = new ArrayList<>() {{
+            add(new ArrayList<>() {{ add(1); add(1); add(0); add(0);}});
+            add(new ArrayList<>() {{ add(1); add(1); add(0); add(1);}});
+            add(new ArrayList<>() {{ add(1); add(0); add(0); add(0);}});
+        }};
+
+        Apriori apriored = new Apriori(test);
+        apriored.show(0.4f, 0.4f, 0.1f);
+        apriored.tree();
     }
 }
